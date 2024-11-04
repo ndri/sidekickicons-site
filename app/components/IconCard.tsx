4f73401ef3fc16ab6aaad948c5bf2cbc48302a71
ndri <5224431+ndri@users.noreply.github.com>
2024-11-04 11:07:22 +0100

    Make copying tab-accessible

--- app/components/IconCard.tsx
+++ app/components/IconCard.tsx
@@ -27,14 +27,17 @@ export default function IconCard({
   const codeToCopy = iconCodeFunctions[codeType](fullHeroicon, type);
 
   return (
-    <div className="group grid grid-cols-1 grid-rows-1 rounded-md bg-slate-50 p-4">
+    <div
+      className="group grid grid-cols-1 grid-rows-1 rounded-md bg-slate-50 p-4"
+      tabIndex={0}
+    >
       <div className="col-span-full row-span-full flex flex-col items-center justify-center group-hover:opacity-25">
         <Icon className={`${iconClasses} text-slate-700`} />
         <p className="mt-2 max-w-full truncate text-xs text-slate-500" title={name}>
           {name}
         </p>
       </div>
-      <div className="invisible z-10 col-start-1 col-end-2 row-start-1 row-end-2 flex flex-col justify-center gap-1 group-hover:visible">
+      <div className="invisible z-10 col-start-1 col-end-2 row-start-1 row-end-2 flex flex-col justify-center gap-1 group-focus-within:visible group-hover:visible">
         <CopyButton textToCopy={codeToCopy} />
         <button
           className="group/button flex justify-center gap-1 rounded bg-white px-2 py-1 text-sm font-semibold text-slate-900 shadow-sm ring-1 ring-inset ring-slate-300 hover:bg-slate-50"
