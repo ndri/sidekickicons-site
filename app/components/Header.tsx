7277672cf9a7283810bb11c1d5eeddb7ca39121b
ndri <5224431+ndri@users.noreply.github.com>
2024-11-04 12:58:36 +0100

    Add mask to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,11 @@
+import { MaskIcon } from "@sidekickicons/react/24/solid";
 export default function Header() {
   return (
     <header className="w-full max-w-6xl p-8">
-      <h1 className="mb-3 text-4xl font-bold text-slate-800">Sidekickicons</h1>
+      <h1 className="mb-3 flex items-center gap-4 text-3xl font-bold text-slate-800">
+        <MaskIcon className="size-12 text-indigo-600" />
+        sidekickicons
+      </h1>
       <p className="text-slate-500">Fanmade icons to complement Heroicons</p>
     </header>
   );
