a75a561e764556592a40e76563fda51ac7b9b584
ndri <5224431+ndri@users.noreply.github.com>
2025-01-01 23:35:31 +0100

    Add suspense fallback spinner

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import { Suspense } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import IconSearch from "../components/IconSearch";
+import Spinner from "@/components/Spinner";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Header />
       <main className="w-full max-w-7xl p-4 sm:p-8">
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
           <IconSearch />
         </Suspense>
       </main>
---
+++ components/Spinner.tsx
@@ -0,0 +1,9 @@
+import { ArcThirdIcon } from "@sidekickicons/react/24/outline";
+
+export default function Spinner() {
+  return (
+    <div className="flex justify-center py-8">
+      <ArcThirdIcon className="size-16 animate-spin text-indigo-600" />
+    </div>
+  );
+}
